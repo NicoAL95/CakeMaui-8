9b09120e6756f46ce2300c6715b3f19696fc744b
Verio Aberossy R <verio.renedominick@gmail.com>
2023-12-17 15:08:59 +0700

    Adding ViewModel

---
+++ CakeMaui/ViewModels/OfficialFormViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CakeMaui.Models;
+using System;
+
+namespace CakeMaui.ViewModels
+{
+    public class OfficialFormViewModel : ObservableObject
+    {
+        private OfficialForm _officialForm;
+
+        public OfficialForm OfficialForm
+        {
+            get { return _officialForm; }
+            set
+            {
+                _officialForm = value;
+                OnPropertyChanged(nameof(OfficialForm));
+            }
+        }
+
+        public OfficialFormViewModel()
+        {
+            OfficialForm = new OfficialForm();
+        }
+
+        public OfficialFormViewModel(OfficialForm officialForm)
+        {
+            OfficialForm = officialForm;
+        }
+    }
+}
